5f5debc1ca58e31fd0f5e522833fefb670540a15
Aaron Zhang <aaronbxzhang@gmail.com>
2025-12-14 14:59:22 -0500

    Add EventCode.java interface for type safety between event codes and type

---
+++ src/eventclassification/eventcodes/EventCode.java
@@ -0,0 +1,8 @@
+package eventclassification.eventcodes;
+
+import eventclassification.EventCategory;
+
+// Empty interface to force type safety
+public interface EventCode extends EventCategory{
+    
+}
