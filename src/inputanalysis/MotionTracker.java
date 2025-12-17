dc9132926374d92dd37921c4ba45ce824beb0bde
Aaron Zhang <aaronbxzhang@gmail.com>
2025-12-16 21:44:47 -0500

    Full implementation of system to get x and y values from input stream

--- src/inputanalysis/MotionTracker.java
+++ src/inputanalysis/MotionTracker.java
@@ -8,8 +8,25 @@ import eventclassification.eventcodes.Rel;
 // to be implemented
 public class MotionTracker implements Runnable {
     private final InputReader reader;
-    private final EventData[] xValues = new EventData[2];
-    private final EventData[] yValues = new EventData[2];
+
+    private final EventData[] xValues = {null, null};
+    private final EventData[] yValues = {null, null};
+
+    private final int initialIndex = 0;
+    private final int finalIndex = 1;
+    
+
+
+    // private EventData xValueFinal;
+    // private EventData xValueInitial;
+
+    // private EventData yValueFinal;
+    // private EventData yValueInitial;
+
+
+
+    // private final EventData[] xValues = new EventData[2];
+    // private final EventData[] yValues = new EventData[2];
 
     public MotionTracker(InputReader reader) {
         this.reader = reader;
@@ -34,12 +51,16 @@ public class MotionTracker implements Runnable {
 
             if (data.eventCode().equals(Rel.REL_X)) {
                 foundXValues++;
-                // xValues[foundXValues] = data;
-                continue;
 
+                xValues[initialIndex] = xValues[finalIndex];
+                xValues[finalIndex] = data;
+                continue;
+                
             } else if (data.eventCode().equals(Rel.REL_Y)) {
                 foundYValues++;
-                // yValues[foundYValues] = data;
+                yValues[initialIndex] = yValues[finalIndex];
+                yValues[finalIndex] = data;
+
             }
 
 
