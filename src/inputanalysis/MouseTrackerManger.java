f022ee20bed52c25d435ae36d99ea2e80c5e0970
Aaron Zhang <aaronbxzhang@gmail.com>
2025-12-31 20:38:25 -0500

    Make thread in the MouseTrackerManager class

--- src/inputanalysis/MouseTrackerManger.java
+++ src/inputanalysis/MouseTrackerManger.java
@@ -2,9 +2,18 @@ package inputanalysis;
 
 public class MouseTrackerManger {
     private MouseMotionTracker mouseTracker;
+    private Thread runner;
 
-    public MouseTrackerManger() {
+    public MouseTrackerManger(MouseMotionTracker mouseTracker) {
+        this.mouseTracker = mouseTracker;
+
+        runner = new Thread(mouseTracker);
 
     }
+
+    public void start() {
+        runner.start();
+    }
+
 
 }
