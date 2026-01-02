488421c5dca4c545a3313b3997bffee5af4dd563
Aaron Zhang <aaronbxzhang@gmail.com>
2026-01-01 23:06:25 -0500

    Add overloaded constructor to EventData record to create a default event report

--- src/devicemanagement/EventData.java
+++ src/devicemanagement/EventData.java
@@ -3,9 +3,16 @@ package devicemanagement;
 import eventclassification.EventTypes;
 import eventclassification.eventcodes.EventCode;
 
-public record EventData(
+public record EventData (
     long[] time,
     EventTypes eventType,
     EventCode eventCode,
     int value
-) {}
+) {
+
+    public EventData() {
+        long[] defaultTime = {0, 0};
+        this(defaultTime, null, null, 0);
+    }
+
+}
