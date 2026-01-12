c3547571fbb9812620fa87378265ddd42a2edb65
Aaron Zhang <102826852+AaronZhang425@users.noreply.github.com>
2026-01-12 11:49:48 -0500

    "Add class skeleton of EventFilter"

--- src/inputanalysis/EventFilter.java
+++ src/inputanalysis/EventFilter.java
@@ -4,11 +4,24 @@ package inputanalysis;
 // This class represents a single filter for events by event type
 // or by both event type and code
 public class EventFilter {
+    private EventTypes eventTypes;
+    private EventCode eventCode;
 
-    public EventFilter() {
+    private Function<EventData, Boolean> filter;
 
+    public EventFilter(EventTypes eventType, EventCode eventCode) {
 
     }
+
+    private void configFilter() {
+
+    }
+
+    public boolean isMatch(EventData eventData) {
+        return filter.apply(eventData);
+   
+    }
+    
 
 
 }
