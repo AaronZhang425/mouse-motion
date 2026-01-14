cbe2cdc3076fbbdfe449e3e2ced22783f62b828f
Aaron Zhang <aaronbxzhang@gmail.com>
2026-01-13 20:40:51 -0500

    Implement constructors to create a single filter in EventFilter class

--- src/inputanalysis/EventFilter.java
+++ src/inputanalysis/EventFilter.java
@@ -4,27 +4,66 @@ import eventclassification.eventcodes.EventCode;
 import eventclassification.EventTypes;
 import devicemanagement.EventData;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.function.*;
 
 // TODO: Implement class
 // This class represents a single filter for events by event type
 // or by both event type and code
 public class EventFilter {
-    private EventTypes eventTypes;
+    private EventTypes eventType;
     private EventCode eventCode;
 
-    private Function<EventData, Boolean> filter;
+    private Function<EventData, Boolean> eventFilter;
 
-    public EventFilter(EventTypes eventType, EventCode eventCode) {
+    public static ArrayList<EventFilter> createFilterGroup(EventTypes eventType, EventCode eventCode) {
+        return new ArrayList<>();
 
     }
 
-    private void configFilter() {
+    public static ArrayList<EventFilter> createFilterGroup(HashMap<EventTypes, EventCode> fullCapabilities) {
+        return new ArrayList<>();
+    
+    }
+
+
+    public EventFilter(EventTypes eventType, EventCode eventCode) {
+        this.eventType = eventType;
+        this.eventCode = eventCode;
+
+        eventFilter = configFilter();
+    }
+
+    public EventFilter(EventTypes eventType) {
+        this(eventType, null);
+    }
+
+    private Function<EventData, Boolean> configFilter() {
+        Function<EventData, Boolean> filter;
+
+        if (eventCode == null) {
+            // filter only by event type
+            filter = eventData -> {
+                return eventData.eventType().equals(eventType);
+            };
+
+        } else {
+            // filter by both event type and event code
+            filter = eventData -> {
+                return (
+                    eventData.eventType().equals(eventType) &&
+                    eventData.eventCode().equals(eventCode)
+                );
+            };
+        }
+
+        return filter;
 
     }
 
     public boolean isMatch(EventData eventData) {
-        return filter.apply(eventData);
+        return eventFilter.apply(eventData);
    
     }
     
