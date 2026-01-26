863401e61ce07ee3d7ed8e50af6469227fc73b70
Aaron Zhang <aaronbxzhang@gmail.com>
2026-01-26 13:37:19 -0500

    Remove unneccesary comments

--- src/devicemanagement/EventData.java
+++ src/devicemanagement/EventData.java
@@ -4,42 +4,6 @@ import eventclassification.EventTypes;
 import eventclassification.eventcodes.EventCode;
 import java.util.Arrays;
 
-// Linux stores event data as a struct
-// 
-// time is represented as an inner struct to represent seconds and fractional
-// seconds in microseconds
-//
-// eventType is represented with an unsigned short
-//
-// eventCode is represented with an unsigned short
-//
-// value is represented with a signed integer
-//
-// The implementation of representing this struct in java is done using an
-// long[] array to represent time and enums for event type and code. The value
-// of an event is stored as an int
-
-// public record EventData (
-//     long[] time,
-//     EventTypes eventType,
-//     EventCode eventCode,
-//     int value
-// ) {
-
-//     public String toString() {
-//         return (
-//             "Input Event Info: " + "\n" +
-//             "Seconds: " + time[0] + "\n" +
-//             "Microseconds: " + time[1] + "\n" +
-//             "Event Type: " + eventType + "\n" +
-//             "Event Code: " + eventCode + "\n" +
-//             "Value: " + value + "\n"
-//         );
-
-//     }
-
-// }
-
 /**
  * This class holds data of each event created by the device. The structure of 
  * the event is outlined by the struct input_event in the file input.h
