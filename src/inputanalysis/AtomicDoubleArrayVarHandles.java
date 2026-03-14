042c530fb939f92d57a47bbd53563f10b24b5919
Aaron Zhang <aaronbxzhang@gmail.com>
2026-03-14 19:02:27 -0400

    Implement set, get and cas methods of atomic double array with  varHandles

--- src/inputanalysis/AtomicDoubleArrayVarHandles.java
+++ src/inputanalysis/AtomicDoubleArrayVarHandles.java
@@ -4,21 +4,46 @@ import java.lang.invoke.VarHandle;
 import java.lang.invoke.MethodHandles;
 
 public class AtomicDoubleArrayVarHandles {
-    private static final VarHandle arrElemHandle;
+    private static final VarHandle ELEM_HANDLE;
 
-    private double[] arr;
+    private final long[] arr;
 
     static {
-        arrElemHandle = MethodHandles.arrayElementVarHandle(double[].class);
+        ELEM_HANDLE = MethodHandles.arrayElementVarHandle(long[].class);
     }
 
     public AtomicDoubleArrayVarHandles(int size) {
-        arr = new double[size];
+        arr = new long[size];
 
     }
 
     public AtomicDoubleArrayVarHandles(double[] arr) {
-        System.arraycopy(arr, 0, this.arr, 0, arr.length);
+        this.arr = new long[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            this.arr[i] = Double.doubleToRawLongBits(arr[i]);
+        
+        }
+
+    }
+
+    public double get(int index) {
+        return Double.longBitsToDouble((long) ELEM_HANDLE.getVolatile(arr, index));
+
+    }
+
+    public void set(int index, double newValue) {
+        ELEM_HANDLE.setVolatile(arr, index, newValue);
+
+    }
+
+    public boolean compareAndSet(int index, double expected, double newValue) {
+        return ELEM_HANDLE.compareAndSet(
+            arr,
+            index,
+            Double.doubleToRawLongBits(expected),
+            Double.doubleToRawLongBits(newValue)
+        );
 
     }
 
