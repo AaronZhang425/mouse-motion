af37d2112a0e37a0dad187082206f3464fcc7c5d
Aaron Zhang <aaronbxzhang@gmail.com>
2026-03-30 19:22:33 -0400

    Add methods to calculate the dot product of the total displacement of 2 mice

--- src/inputanalysis/MouseSystem.java
+++ src/inputanalysis/MouseSystem.java
@@ -17,6 +17,36 @@ public class MouseSystem {
         
     }
 
+    /**
+     * Gets the dot product of the total displacement of the mice
+     * 
+     * @param mouse1 A mouse tracker
+     * @param mouse2 A mouse tracker representing another mouse
+     * @return Dot product of the total displacement of the mice
+     */
+    private double getTotalDisplacementDotProduct(
+        MouseMotionTracker mouse1,
+        MouseMotionTracker mouse2
+    ) {
+        return getDotProduct(
+            mouse1.getDisplacement(),
+            mouse2.getDisplacement()
+        );
+
+    }
+
+    /**
+     * Gets the dot product of 2 vectors that are 2D
+     * 
+     * @param vector1
+     * @param vector2
+     * @return
+     */
+    private double getDotProduct(double[] vector1, double[] vector2) {
+        return vector1[0] * vector2[0] + vector1[1] + vector2[1];
+
+    }
+
     
 
 }
